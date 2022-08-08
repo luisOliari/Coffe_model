a71be7bc3af02082aac4d2674f4e048076e5c089
luisOliari <luisoliari@yahoo.com>
2022-08-08 22:37:44 +0000

    cambios

--- src/app.py
+++ src/app.py
@@ -1,22 +1,30 @@
-from flask import Flask, render_template, jsonify, request 
+from flask import Flask, render_template, jsonify, request
+import pickle
+import pandas as pd
+import numpy as np
 
-app = Flask(__name__) # hacer ref al nombre del archivo
+app = Flask(__name__)
 
 @app.route('/')
-def hello_flask():
-    return 'Hello Flask'
+def hello_flask(): 
+    return 'Hello flask'
 
-@app.route('/inicio')
+@app.route('/inicio') #renderiza página web
 def show_home():
-    return render_template('Index.html')
+    return render_template('index.html')
 
-@app.route('/url_variables/<string: name>/<int: age>')
-def url_variables(name,age):
-    if age < 18:
-        return jsonify(message = 'Lo siento' + name + 'no estas autorizado'), 401
+@app.route('/<string: country>/<string: variety>/<float: aroma>/<float: aftertaste>/<float: acidity>/<float: body>/<float: balance>/<float: moisture>')
+def result(country, variety, aroma, aftertaste, acidity, body, balance, moisture):
+    cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
+    data = [country, variety, aroma, aftertaste, acidity, body, balance, moisture]
+    posted = pd.DataFrame(np.array(data).reshape(1,8), columns=cols)
+    loaded_model = pickle.load(open('../models/coffee_model.pkl', 'rb')) #rb es read binary
+    result = loaded_model.predict(posted)
+    text_result = result.tolist()[0]
+    if text_result == 'Yes':
+        return jsonify(message = 'Your coffee is great!'), 200
     else:
-        return jsonify(message = 'Bienvenido' + name), 200 # por default es 200
-    
-if __name__ == '__main__':
-    app.run(debug= True, host='127.0.0.1', port=5000) 
+        return jsonify(message = 'Your coffee sucks!'), 200
 
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
